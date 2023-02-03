57f93276681b0fa2b4a01c7c989d9dcc9f50de77
jeferson <jefinho368@gmail.com>
2023-02-03 08:53:31 -0300

    Update TiposVariaveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Sr Wick!");
+        // tipos primitivos
+        //classes String que representam um texto na aplicação
+
+        byte idade = 123;
+        short ano = 2023;
+        int cep = 21070333; // se começar com zero
+        long cpf = 9876543219L;
+        //float pi = 3.14F; 
+        double salarioMinimo  = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //adaptando o casting
+
+        int numero = 5;
+
+        numero = 10;
+        System.out.println("Este número agora é " + numero);
+
+        final double VALOR_DE_PI = 3.14; // Não pode ser alterada
+
+
     }
 }
